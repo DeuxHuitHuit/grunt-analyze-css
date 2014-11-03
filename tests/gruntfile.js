68c60e9368094de12cd4e88f57b93b47ea6d9acb
Nicolas Brassard <nico@nitriques.com>
2014-11-03 18:31:38 -0500

    Fixed crazy indentation

--- tests/gruntfile.js
+++ tests/gruntfile.js
@@ -1,27 +1,27 @@
 /*global module*/
 module.exports = function (grunt) {
-    'use strict';
+	'use strict';
 
-    grunt.initConfig({
-        analyzecss: {
-            prod: {
-                sources: [
-                    'empty.css',
-                    'bootstrap.css',
-                    'ie.css',
-                    'symphony.css'
-                ]
-            },
-            options: {
-                outputMetrics: 'error',
-                thresholds: {
-                    
-                }  
-            }
-        }
-    });
-    
-    grunt.loadTasks('../tasks');
+	grunt.initConfig({
+		analyzecss: {
+			prod: {
+				sources: [
+					'empty.css',
+					'bootstrap.css',
+					'ie.css',
+					'symphony.css'
+				]
+			},
+			options: {
+				outputMetrics: 'error',
+				thresholds: {
+					
+				}
+			}
+		}
+	});
+	
+	grunt.loadTasks('../tasks');
 
-    grunt.registerTask('default', ['analyzecss']);
+	grunt.registerTask('default', ['analyzecss']);
 };
